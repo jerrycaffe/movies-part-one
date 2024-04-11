130d11dbc164b9f908a375f29d3bfb6ea5197537
jerrycaffe <jerrycaffe@gmail.com>
2024-04-11 09:07:21 +0100

    first commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization") version "1.9.23"
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+
+            api(libs.koin.core)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            implementation(libs.io.ktor.ktor.client.android)
+
+            api(libs.koin.android)
+        }
+        iosMain.dependencies {
+            implementation(libs.io.ktor.ktor.client.android)
+
+            implementation(libs.ktor.client.darwin)
+        }
+    }
+}
+
+android {
+    namespace = "com.example.movies"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/movies/data/remote/KtorApi.kt
@@ -0,0 +1,34 @@
+package com.example.movies.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.parameter
+import io.ktor.http.path
+import io.ktor.http.takeFrom
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+private const val BASE_URL = "https://api/themoviedb.org/"
+private const val API_KEY = "f825cd6c7465821dca2a1f1734f4eaf6"
+
+internal abstract class KtorApi {
+    val client = HttpClient {
+        val client = HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    useAlternativeNames = false
+                })
+            }
+        }
+    }
+
+    fun HttpRequestBuilder.pathUrl(path: String) {
+        url {
+            takeFrom(BASE_URL)
+            path("3", path)
+            parameter("api_key", API_KEY)
+        }
+    }
+}
